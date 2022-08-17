130c44af51473dc43c547bb22b2af8bdf37a2f5a
kokor <kokor@DESKTOP-7U0ABC6>
2022-08-17 13:20:40 +0500

    Добавьте файлы проекта.

---
+++ лаба 1.2/лаба 1.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	double n, m;
+	cout << " Vvedite n ";
+	cin >> n;
+	cout << " Vvedite m ";
+	cin >> m;
+	cout << " Znachenie virajeniya n+++m=" << n+ ++m;
+	cout << " Znachenie virajeniya m-- >n=" << (m-- > n);
+	cout << " Znachenie virajeniya n-- >m=" << (n-- > m);
+	return 0;
+}
+
